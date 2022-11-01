9b09532913f68459d6c58515cfed5656dbc77381
Andrey Villegas <villegas715@gmail.com>
2022-11-01 08:44:18 -0600

    styling login screen

--- gg_shop/src/app/screens/login.js
+++ gg_shop/src/app/screens/login.js
@@ -2,9 +2,43 @@ import '../styles/login.css';
 
 export function Login () {
 return (
-    <div>
-        <h1>hola mundo</h1>
-    </div>
+    
+    <body>
+    <form class="formulario">
+       
+       <div class="logo">
+
+      <h1>logo</h1> 
+      
+      </div>
+      
+       <div class="contenedor">
+
+        <div class="input-contenedor">
+            <p>Correo</p>
+            <input type="text" placeholder="correo"></input> 
+
+        </div>
+
+        <div class="input-contenedor">
+            <p>Contraseña</p>
+            <input type="password" placeholder="contraseña"></input> 
+            <p class="texto"><a class="link">Olvide Mi Contraseña</a></p>
+
+        </div>
+         
+         <input type="submit" value="login" class="button"></input>
+         <input type="submit" value="Register" class="button"></input>
+         
+       </div>
+     </form>  
+        
+
+            
+
+    
+
+    </body>
 );
 
 
